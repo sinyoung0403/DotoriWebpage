c18fecea261f8450f8baf9245c9f0f6a192593aa
sinyoung0403 <cinn1004@naver.com>
2025-02-19 20:48:13 +0900

    API 형태로 바꾸기

--- js/dailyScrum.js
+++ js/dailyScrum.js
@@ -21,6 +21,99 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
+// 데이터 불러오기
+export async function fetchData() {
+    const response = await fetch(`https://firestore.googleapis.com/v1/projects/tododata-e3181/databases/(default)/documents/Scrum`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error("데이터를 불러오는데 실패했습니다.");
+    }
+
+    const data = await response.json();
+    return data; // 데이터를 반환
+}
+
+// 데이터 load 하기
+async function loadData() {
+    try {
+        const data = await fetchData(); // fetchData 호출
+        if (data.documents) {
+            data.documents.forEach(doc => {
+                const fields = doc.fields;
+                let teamAim = fields.teamAim.stringValue
+                let ghGoal = fields.ghGoal.stringValue
+                let cjGoal = fields.cjGoal.stringValue
+                let syGoal = fields.syGoal.stringValue
+                let id = doc.name.split("/").pop();
+                let date = fields.date?.stringValue
+                let ghcheck = fields.ghcheck.stringValue
+                let cjcheck = fields.cjcheck.stringValue
+                let sycheck = fields.sycheck.stringValue
+                let temp_html = ``
+                temp_html =
+                    `   <div class="accordion-item" id="${id}">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${id}" aria-expanded="false" aria-controls="collapse${id}">
+                                    # ${date} ] 데일리 스크럼
+                                </button>
+                            </h2>
+                            <div id="collapse${id}" class="accordion-collapse collapse" >
+                                <div class="accordion-body">
+                                    <div>
+                                        <h5>오늘의 팀 목표</h5>
+                                        <p>${teamAim}</p>
+                                    </div>
+                                    <br>
+                                    <div>
+                                        <h5>개별 목표 달성 여부</h5>
+                                        <div  id="goalGap"></div>
+                                        <h6>규현님 목표) </h6>
+                                        <div class="form-check stack-check">
+                                            <input class="form-check-input ghcheckbox" type="checkbox" value="" id="flexCheckDefault" style="pointer-events: none;" ${ghcheck}>
+                                            <label class="form-check-label" for="flexCheckDefault">
+                                                ${ghGoal}
+                                            </label>
+                                        </div>
+                                        <h6>채진님 목표) </h6>
+                                        <div class="form-check stack-check">
+                                            <input class="form-check-input cjcheckbox"  type="checkbox" value="" id="flexCheckDefault" style="pointer-events: none;" ${cjcheck} >
+                                            <label class="form-check-label" for="flexCheckDefault">
+                                                ${cjGoal}
+                                            </label>
+                                        </div>
+                                        <h6>신영님 목표) </h6>
+                                        <div class="form-check stack-check">
+                                            <input class="form-check-input sycheckbox" type="checkbox" value="" id="flexCheckDefault" style="pointer-events: none;" ${sycheck}>
+                                            <label class="form-check-label" for="flexCheckDefault">
+                                                ${syGoal}
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                        <button id="scrumEditBtn" class="btn btn-warning" type="button">수정하기</button>
+                                        <button id="scrumDeleteBtn" class="btn btn-warning" type="button" data-bs-toggle="modal" data-bs-target="#deleteScrumModal">삭제하기</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                `
+                $("#accordionExample").append(temp_html);
+            });
+        } else {
+            console.log("문서 없음");
+        }
+    } catch (error) {
+        console.error("오류 발생:", error);
+    }
+}
+loadData();
+
+
 // 날씨 환산 함수
 function getTodayDate() {
     let today = new Date();
@@ -29,92 +122,42 @@ function getTodayDate() {
     return `${month < 10 ? '0' + month : month}월 ${day < 10 ? '0' + day : day}일`;
 }
 
-// DB 에 보내기
+// 데이터 추가하기기
 $("#addScrumBtn").click(async function () {
-    let teamAim = $('#teamAim').val();
-    let ghGoal = $('#ghGoal').val();
-    let cjGoal = $('#cjGoal').val();
-    let syGoal = $('#syGoal').val();
-    let customId = Date.now().toString();
-    let date = getTodayDate();
-
-    const docRef = doc(db, "Scrum", customId);
-    await setDoc(docRef, {
-        id: customId,
-        teamAim: teamAim,
-        ghGoal: ghGoal,
-        cjGoal: cjGoal,
-        syGoal: syGoal,
-        ghcheck: " ",
-        cjcheck: " ",
-        sycheck: " ",
-        date: date
-    });
-    window.location.reload();
-})
-
-// 데이터 띄우기
-let scrums = await getDocs(collection(db, "Scrum"));
-scrums.forEach(scrum => {
-    let teamAim = scrum.data()['teamAim']
-    let ghGoal = scrum.data()['ghGoal']
-    let cjGoal = scrum.data()['cjGoal']
-    let syGoal = scrum.data()['syGoal']
-    let id = scrum.data()['id']
-    let date = scrum.data()['date']
-    let ghcheck = scrum.data()['ghcheck']
-    let cjcheck = scrum.data()['cjcheck']
-    let sycheck = scrum.data()['sycheck']
-    let temp_html = ``
-
-    temp_html =
-        `   <div class="accordion-item" id="${id}">
-                <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${id}" aria-expanded="false" aria-controls="collapse${id}">
-                        # ${date} ] 데일리 스크럼
-                    </button>
-                </h2>
-                <div id="collapse${id}" class="accordion-collapse collapse" >
-                    <div class="accordion-body">
-                        <div>
-                            <h5>오늘의 팀 목표</h5>
-                            <p>${teamAim}</p>
-                        </div>
-                        <br>
-                        <div>
-                            <h5>개별 목표 달성 여부</h5>
-                            <div  id="goalGap"></div>
-                            <h6>규현님 목표) </h6>
-                            <div class="form-check stack-check">
-                                <input class="form-check-input ghcheckbox" type="checkbox" value="" id="flexCheckDefault" style="pointer-events: none;" ${ghcheck}>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    ${ghGoal}
-                                </label>
-                            </div>
-                            <h6>채진님 목표) </h6>
-                            <div class="form-check stack-check">
-                                <input class="form-check-input cjcheckbox"  type="checkbox" value="" id="flexCheckDefault" style="pointer-events: none;" ${cjcheck} >
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    ${cjGoal}
-                                </label>
-                            </div>
-                            <h6>신영님 목표) </h6>
-                            <div class="form-check stack-check">
-                                <input class="form-check-input sycheckbox" type="checkbox" value="" id="flexCheckDefault" style="pointer-events: none;" ${sycheck}>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    ${syGoal}
-                                </label>
-                            </div>
-                        </div>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button id="scrumEditBtn" class="btn btn-warning" type="button">수정하기</button>
-                            <button id="scrumDeleteBtn" class="btn btn-warning" type="button" data-bs-toggle="modal" data-bs-target="#deleteScrumModal">삭제하기</button>
-                        </div>
-                    </div>
-                </div>
-            </div>    
-    `
-    $("#accordionExample").append(temp_html);
+    const data = {
+        fields: {
+            teamAim: { stringValue: $('#teamAim').val() },  // 필드 이름이 정확한지 확인
+            ghGoal: { stringValue: $('#ghGoal').val() },
+            cjGoal: { stringValue: $('#cjGoal').val() },
+            syGoal: { stringValue: $('#syGoal').val() },
+            id: { stringValue: Date.now().toString() }, // 내가 지정한 ID 값
+            date: { stringValue: getTodayDate() },
+            ghcheck: { stringValue: " " },
+            cjcheck: { stringValue: " " },
+            sycheck: { stringValue: " " }
+        }
+    };
+    // POST 요청으로 문서 추가
+    fetch("https://firestore.googleapis.com/v1/projects/tododata-e3181/databases/(default)/documents/Scrum", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-goog-api-key": "AIzaSyC_AWLt1X27LrQB9j4H67Zf0N5v0Hc4Vig"
+        },
+        body: JSON.stringify(data)  // 데이터를 JSON으로 변환하여 전송
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.error("Error: ", response.statusText);
+            return response.text(); // 응답을 텍스트로 읽기
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("문서 추가 성공:", data);
+    })
+    .catch(error => console.error("데이터 추가 실패:", error));
+    location.reload(); // 페이지 새로고침
 });
 
 // 삭제 버튼을 클릭했을 시,
@@ -145,7 +188,7 @@ $(document).on("click", "#scrumDeleteBtn", async function () {
 $(document).on("click", "#scrumEditBtn", async function () {
     // 1. 해당 버튼을 누른 요소의 상위 id 값을 저장해야함.
     let id = $(this).parent().parent().parent().parent().attr("id");
-    // 2. id 말고 다른 것도 불러오자. get 으로 .. 다 땡겨오자.
+    // 2. id 말고 다른 것도 불러오자. get 으로 다 땡겨오자.
     fetch(`https://firestore.googleapis.com/v1/projects/tododata-e3181/databases/(default)/documents/Scrum/${id}`, {
         method: "GET",
         headers: {
@@ -159,13 +202,14 @@ $(document).on("click", "#scrumEditBtn", async function () {
             const date = data.fields.date.stringValue;
             const ghGoal = data.fields.ghGoal.stringValue;
             const ghcheck = data.fields.ghcheck.stringValue;
+            const document_id = data.name.split("/").pop();
             const id = data.fields.id.stringValue;
             const syGoal = data.fields.syGoal.stringValue;
             const sycheck = data.fields.sycheck.stringValue;
             const teamAim = data.fields.teamAim.stringValue;
-
+            
             let temp_html = `
-            <div class="mypostingbox" id="editBox" data-value="${id}">
+            <div class="mypostingbox" id="editBox" data-value="${document_id}">
                 <h3>데일리 스크럼 수정</h3>
                 <br>
                 <h3>오늘의 팀 목표</h3>
@@ -218,16 +262,15 @@ $(document).on("click", "#scrumEditBtn", async function () {
 
 // 실제 수정하기 버튼을 눌렀을 시, 데이터를 Edit 해주기
 $(document).on("click", "#EditBtn", async function () {
-    let id = $("#editBox").data("value");
+    let id = $("#editBox").data("value").toString();
     let teamAim = $("#editteamAim").val();
     let editghGoal = $("#editghGoal").val();
     let editcjGoal = $("#editcjGoal").val();
     let editsyGoal = $("#editsyGoal").val();
-    let date = $("#editBoxdatevalue").data("value");
+    let date = $("#editBoxdatevalue").data("value").toString();
     let ghcheck = " ";
     let cjcheck = " ";
     let sycheck = " ";
-    console.log(date);
     if ($("#ghEditCheckbox").prop("checked")) {
         ghcheck = "checked";
     }
@@ -237,31 +280,12 @@ $(document).on("click", "#EditBtn", async function () {
     if ($("#syEditCheckbox").prop("checked")) {
         sycheck = "checked";
     }
-    console.log(sycheck)
 
-    // json 형태로 저장해두기
-    const fieldsToUpdate = {
-        fields: {
-            date: { stringValue: date },
-            teamAim: { stringValue: teamAim },
-            ghGoal: { stringValue: editghGoal },
-            cjGoal: { stringValue: editcjGoal },
-            syGoal: { stringValue: editsyGoal },
-            ghcheck: { stringValue: ghcheck },
-            cjcheck: { stringValue: cjcheck },
-            sycheck: { stringValue: sycheck }
-        }
-    };
-
-
-    // 업데이트할 필드 목록 만들기
-    const updateMask = Object.keys(fieldsToUpdate)
-        .map(field => `fields.${field}`)
-        .join(",");
 
     $(document).on("click", "#editScrumBtn", async function () {
         // PATCH 요청으로 데이터 업데이트
-        fetch(`https://firestore.googleapis.com/v1/projects/tododata-e3181/databases/(default)/documents/Scrum/${id}?` + `updateMask.fieldPaths=date&updateMask.fieldPaths=teamAim&updateMask.fieldPaths=ghGoal&` +
+        fetch(`https://firestore.googleapis.com/v1/projects/tododata-e3181/databases/(default)/documents/Scrum/${id}?` +
+             `updateMask.fieldPaths=date&updateMask.fieldPaths=teamAim&updateMask.fieldPaths=ghGoal&` +
             `updateMask.fieldPaths=cjGoal&updateMask.fieldPaths=syGoal&updateMask.fieldPaths=ghcheck&` +
             `updateMask.fieldPaths=cjcheck&updateMask.fieldPaths=sycheck`, {
             method: "PATCH",
